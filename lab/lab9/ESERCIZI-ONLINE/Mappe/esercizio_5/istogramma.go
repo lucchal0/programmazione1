d8fa9183c98ddac330d6b4c3091efdffbaf2e23b
Alessandro Lucchesi <118201498+lucchal0@users.noreply.github.com>
2024-12-02 15:51:30 +0100

    lezione 2 dicembre

--- lab/lab9/ESERCIZI-ONLINE/Mappe/esercizio_5/istogramma.go
+++ lab/lab9/ESERCIZI-ONLINE/Mappe/esercizio_5/istogramma.go
@@ -20,8 +20,8 @@ func occorenze(s string) map[rune]int {
 
 	m := map[rune]int{}
 	for _, lettera := range s {
-		if unicode.IsLetter(lettera){
-		m[lettera] += 1
+		if unicode.IsLetter(lettera) {
+			m[lettera] += 1
 		}
 	}
 
@@ -29,14 +29,25 @@ func occorenze(s string) map[rune]int {
 
 }
 
+func stampaoccorenze(m map[rune]int) {
+	fmt.Println("istogramma:")
+	for k, v := range m {
+		fmt.Printf("%c: %s\n", k, creaBarra(v))
+	}
+}
+
+func creaBarra(k int) string {
+	s := ""
+
+	for i := 1; i <= k; i++ {
+		s += "*"
+	}
+	return s
+}
+
 func main() {
 
 	//fmt.Println(leggiTesto())
 
-	m := occorenze(leggiTesto())
-
-	for k, v := range m {
-		fmt.Printf("%c -> %d\n", k, v)
-	}
-
+	stampaoccorenze(occorenze(leggiTesto()))
 }
