89380c9f4949b56bf308a541430cdd3a01004a07
Alessandro Lucchesi <118201498+lucchal0@users.noreply.github.com>
2024-12-04 11:35:07 +0100

    4 dicembre

--- teoria/esempi_Classe/geometria2.go
+++ teoria/esempi_Classe/geometria2.go
@@ -5,44 +5,42 @@ import (
 	"math"
 )
 
-func distanza(A, B struct {
+// type Punto = struct { // -> find replace
+// 	x         float64
+// 	y         float64
+// 	etichetta string
+// }
+
+type Punto struct { // -> nuovo nome di tipo
 	x         float64
 	y         float64
 	etichetta string
-}) (distanza_euclidea float64) {
+}
 
+type celsius int32
+type kelvin int32
+
+func distanza(A, B Punto) (distanza_euclidea float64) {
 	distanza_euclidea = math.Sqrt((A.x-B.x)*(A.x-B.x) + (A.y-B.y)*(A.y-B.y))
 	return
 
 }
 func main() {
 
-	var A struct {
+	var A Punto
+	var B struct { // -> nuovo nome di tipo
 		x         float64
 		y         float64
 		etichetta string
 	}
-	var B struct {
-		x         float64
-		y         float64
-		etichetta string
-	}
-
-	/* letterali
-
-	struct
-	A= struct{x float64; y float64; etichetta string}{x:0.0, y:1.0, etichetta: "struct A"}
-
-	var M map[string]int
-	M = ma[string]int{"alberto": 33, "silvia":52}
-
-
-	*/
 
 	A.x = 0.0
 	B.x = 0.0
 	A.y = 1.0
 	B.y = 1.0
+
+	// var c celsius
+	// var k kelvin
 
 	B = A
 
